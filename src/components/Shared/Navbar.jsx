d8d892d486354604e359054c3caa7711cfdbbe34
sabrina <sabrinaahmednitu@gmail.com>
2023-12-16 23:55:16 +0600

    navbar setup

--- src/components/Shared/Navbar.jsx
+++ src/components/Shared/Navbar.jsx
@@ -1,12 +1,73 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar/Navbar.css';
 
 const Navbar = () => {
-    return (
-        <div>
-            <h2>navbar</h2>
-            <h3>i am new</h3>
+  const navItem = (
+    <>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/availablepuppies">Available Puppies</Link>
+      </li>
+      <li>
+        <Link to="/aboutus">About Us</Link>
+      </li>
+      <li>
+        <Link to="/contactus">Contact Us</Link>
+      </li>
+    </>
+  );
+  return (
+    <div>
+      <div>
+        <div className="navbar bg-[#e6e7eb] text-black font-bold ">
+          {/* nav-1 */}
+          <div className="navbar-start">
+            <div className="dropdown">
+              <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5 "
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h8m-8 6h16"
+                  />
+                </svg>
+              </label>
+              <ul
+                tabIndex={0}
+                className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-white rounded-box w-52"
+              >
+                {navItem}
+              </ul>
+            </div>
+            <a className="normal-case text-xl">SweetPaws</a>
+          </div>
+
+          {/* nav-2 */}
+          <div className="navbar-center hidden lg:flex">
+            <ul className="menu menu-horizontal px-1">{navItem}</ul>
+          </div>
+
+          {/* nav-3 */}
+          <div className="navbar-end mx-auto ">
+            {/* <p>{user?.displayName}</p> */}
+            <Link>
+              <h2>Login</h2>
+            </Link>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
