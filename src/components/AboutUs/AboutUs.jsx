71b7e9a4fa3e3aaff12352f935ba94432ab9edb6
sabrina <sabrinaahmednitu@gmail.com>
2023-12-19 02:36:42 +0600

    puppy about sweetpaws done

--- src/components/AboutUs/AboutUs.jsx
+++ src/components/AboutUs/AboutUs.jsx
@@ -4,7 +4,47 @@ import './AboutUs.css'
 const AboutUs = () => {
     return (
       <div>
-        <h2>AboutUs AboutUs</h2>
+        <section>
+          <div className="about-header">
+            <div className="about-header-content">
+              <h2>
+                <strong>Our mission</strong> is to make{' '}
+                <strong>lives better</strong> by placing{' '}
+                <strong>healthy puppies</strong> in ,
+                <strong>happy homes</strong>
+              </h2>
+            </div>
+          </div>
+        </section>
+        <section className="container mx-auto">
+          <div className="hero-section">
+            <div className="hero-header">
+              <h1>About SweetPaws</h1>
+              <p>
+                We are a community of dog lovers, committed to connecting the
+                nation’s top breeders to caring, responsible individuals and
+                families. We hold ourselves and our customers to the highest
+                standards and aim to improve the life of each puppy, breeder and
+                owner who joins our family.
+              </p>
+            </div>
+          </div>
+        </section>
+        <section>
+          <div className="about-placed-container">
+            <div className="about-placed">
+              <div className="placed-1">
+                <img
+                  src="https://cdn.buttercms.com/t3WV6cnQuSjBz6lZWs3E"
+                  alt=""
+                />
+              </div>
+              <div className="placed-2">
+                We have placed over 220,000 puppies into over 220,000 homes.
+              </div>
+            </div>
+          </div>
+        </section>
       </div>
     );
 };
