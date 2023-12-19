a0eebf34f965634e3e6f7802469f74598859e9f6
sabrina <sabrinaahmednitu@gmail.com>
2023-12-19 23:48:24 +0600

    puppy about coverage

--- src/components/AboutUs/AboutUs.jsx
+++ src/components/AboutUs/AboutUs.jsx
@@ -45,6 +45,49 @@ const AboutUs = () => {
             </div>
           </div>
         </section>
+        <section>
+          <div>
+            <h1>Coverage</h1>
+            <div className="coverage-container">
+              <div className="coverage-item">
+                <p>April 14, 2022</p>
+                <h2>The Marketplace 100: 2022</h2>
+                <p>A16Z FUTURE</p>
+              </div>
+              <div className="coverage-item">
+                <p>April 1, 2022</p>
+                <h2>
+                  Two-thirds of Americans claim they spare no expense when it
+                  comes to their furry companions
+                </h2>
+                <p>YAHOO! NEWS</p>
+              </div>
+              <div className="coverage-item">
+                <p>February 20, 2022</p>
+                <h2>
+                  Make-A-Wish Southern Florida gifts 2-year-old with
+                  Goldendoodle puppy
+                </h2>
+                <p>CBS TAMPA BAY</p>
+              </div>
+              <div className="coverage-item">
+                <p>February 3, 2022</p>
+                <h2>
+                  Make-A-Wish, PuppySpot surprise 8-year-old South Jersey girl
+                  with puppy
+                </h2>
+                <p>ABC PHILADELPHIA</p>
+              </div>
+              <div className="coverage-item">
+                <p>January 28, 2022</p>
+                <h2>
+                  6 Major Companies That Offer Pawternity Leave for Pet Parents
+                </h2>
+                <p>GOBANKINGRATES</p>
+              </div>
+            </div>
+          </div>
+        </section>
       </div>
     );
 };
