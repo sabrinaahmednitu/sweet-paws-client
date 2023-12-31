171ebe02a329b961da3a2282fef955afabe643c2
sabrina <sabrinaahmednitu@gmail.com>
2023-12-31 23:44:55 +0600

    sot solution container done

--- src/components/PetInsurance/PetInsurance.jsx
+++ src/components/PetInsurance/PetInsurance.jsx
@@ -7,6 +7,48 @@ const PetInsurance = () => {
           <div className="insurance-banner">
             <h1>Be there for them, like they are for you.</h1>
           </div>
+          <section>
+            <div className="got-solution-container">
+              <div className="got-solution-content">
+                <h1>Got a situation? We’ve got a solution.</h1>
+                <div className="solution-cards-flex">
+                  <div className="solution-card">
+                    <img
+                      src="https://v.fastcdn.co/u/de99de39/64417496-0-AKC-Icon-Injured.svg"
+                      alt=""
+                    />
+                    <h1>WHEN THEY'RE INJURED</h1>
+                    <h4>
+                      We help support them with emergency care, surgery & rehab
+                      coverage.
+                    </h4>
+                  </div>
+                  <div className="solution-card">
+                    <img
+                      src="https://v.fastcdn.co/u/de99de39/64417492-0-AKC-Icon-Sick.svg"
+                      alt=""
+                    />
+                    <h1>WHEN THEY'RE SICK</h1>
+                    <h4>
+                      We provide coverage for eligible claims related to
+                      diagnostics, treatment & medicine.
+                    </h4>
+                  </div>
+                  <div className="solution-card">
+                    <img
+                      src="https://v.fastcdn.co/u/de99de39/64417494-0-AKC-Icon-Healthy.svg"
+                      alt=""
+                    />
+                    <h1>WHEN THEY'RE HEALTHY</h1>
+                    <h4>
+                      We help them stay healthy with optional add-on coverage
+                      for exams, vaccines & check-ups.
+                    </h4>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
         </div>
       </div>
     );
