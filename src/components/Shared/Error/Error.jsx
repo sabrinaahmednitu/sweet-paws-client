653274bf1efe82728daf897afca48a1a038f0c07
sabrina <sabrinaahmednitu@gmail.com>
2024-01-08 13:20:00 +0600

    error page uploaded and helmet also attached

--- src/components/Shared/Error/Error.jsx
+++ src/components/Shared/Error/Error.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
-
+import Helmet from 'react-helmet';
+import './Error.css';
+import error from '../../../images/error-404.jpg'
 const Error = () => {
     return (
-        <div>
-            <h2>error</h2>
+      <div>
+        <Helmet>
+          <meta charSet="utf-8" />
+          <title>Error - SweetPaws</title>
+          <link rel="canonical" href="http://mysite.com/example" />
+        </Helmet>
+        <div className="error-main">
+          <div>
+            <img src={error} alt="" />
+          </div>
         </div>
+      </div>
     );
 };
 
